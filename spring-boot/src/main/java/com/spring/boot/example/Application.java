45a81af9b27dc38b2ed764a7ec6aad5e8b25917f
zenggt <93948886@qq.com>
2017-08-01 17:41:22 +0800

    restruct

--- spring-boot/src/main/java/com/spring/boot/example/Application.java
+++ spring-boot/src/main/java/com/spring/boot/example/Application.java
@@ -2,11 +2,10 @@ package com.spring.boot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
